3fa1ce5680f3234615b18f998055172c5bd231fa
LordAldi <aldianugra09@gmail.com>
2020-12-27 22:13:29 +0700

    finishing error handling

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,9 +1,14 @@
-export class DatabaseConnectionError extends Error {
+import { CustomError } from "./custom-error";
+export class DatabaseConnectionError extends CustomError {
+  statusCode = 500;
   reason = "Error Connecting to database";
   constructor() {
-    super();
+    super("Error Connecting to db...");
 
     //Only because we are extending a built in classs
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+  serializeErrors() {
+    return [{ message: this.reason }];
   }
 }
--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,10 +1,17 @@
 import { ValidationError } from "express-validator";
+import { CustomError } from "./custom-error";
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
+  statusCode = 400;
   constructor(public errors: ValidationError[]) {
-    super();
+    super("Invalid Request Parameters");
 
     //Only because we are extending a built in classs
     Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+  serializeErrors() {
+    return this.errors.map((err) => {
+      return { message: err.msg, field: err.param };
+    });
   }
 }

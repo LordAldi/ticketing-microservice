3971a88644d722d1d6d452dbaae58125849f8dc7
LordAldi <aldianugra09@gmail.com>
2020-12-28 02:12:58 +0700

    add static properties to userModel

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,19 @@
 import mongoose from "mongoose";
 
+// An Interface that describes the properties
+//that required to create new User
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+//an interface taht describe the properties
+//that user model has
+
+interface UserModel extends mongoose.Model<any> {
+  build(attrs: UserAttrs): any;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -11,6 +25,11 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model("User", userSchema);
+//build User for typescript with mongoose so typescipt can check the type
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<any, UserModel>("User", userSchema);
 
 export { User };

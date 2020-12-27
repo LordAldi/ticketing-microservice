9eb3002a8bf64c5fc7181bb25faa495307a59b27
LordAldi <aldianugra09@gmail.com>
2020-12-28 03:29:52 +0700

    add password class to hash and compare, work on singup (almosh finish)

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password";
 
 // An Interface that describes the properties
 //that required to create new User
@@ -10,10 +11,17 @@ interface UserAttrs {
 //an interface taht describe the properties
 //that user model has
 
-interface UserModel extends mongoose.Model<any> {
-  build(attrs: UserAttrs): any;
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
 }
 
+//an interface that describes the properties
+//that a user document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+  //   updatedAt:string
+}
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -24,12 +32,19 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+  done();
+});
 
 //build User for typescript with mongoose so typescipt can check the type
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
-const User = mongoose.model<any, UserModel>("User", userSchema);
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
 
 export { User };

5caba609347b4246b536d37c80fb7152ead9a931
LordAldi <aldianugra09@gmail.com>
2020-12-29 21:39:25 +0700

    add buildClient function

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,27 +1,17 @@
-import axios from "axios";
+import builClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  return <h1>Landing page</h1>;
+  return currentUser ? (
+    <h1>You are singed in {currentUser.email}</h1>
+  ) : (
+    <h1>You are not singed in</h1>
+  );
 };
 
-LandingPage.getInitialProps = async () => {
-  if (typeof window === "undefined") {
-    //on server
-    const { data } = await axios.get(
-      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-      {
-        headers: {
-          Host: "ticketing.dev",
-        },
-      }
-    );
-    return data;
-  } else {
-    //on browser
-    const { data } = await axios.get("/api/users/currentuser");
-    return data;
-  }
-  return {};
+LandingPage.getInitialProps = async (context) => {
+  const client = builClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+
+  return data;
 };
 export default LandingPage;

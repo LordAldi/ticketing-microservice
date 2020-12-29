c5614ebf0bdd1bfc48e10f5e23f4f94016330499
LordAldi <aldianugra09@gmail.com>
2020-12-30 05:58:03 +0700

    create common package forfile sharing

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,8 +1,7 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
-import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest, BadRequestError } from "@anuisme/common-ticket";
 import jwt from "jsonwebtoken";
-import { validateRequest } from "../middlewares/validate-request";
 import { User } from "../models/user";
 import { PasswordManager } from "../services/passwordManager";
 

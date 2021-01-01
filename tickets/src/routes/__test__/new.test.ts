e56af247fc2fffae0528bbad0371cb903cbf35ba
LordAldi <aldianugra09@gmail.com>
2021-01-02 03:29:31 +0700

    create tickets service (initial, not finished)

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,18 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("has a route handler listening to /api/tickets for post rewuest ", async () => {
+  const response = await request(app).post("/api/tickets").send({});
+
+  expect(response.status).not.toEqual(404);
+});
+it("can only be access if the user signed in", async () => {
+  const response = await request(app).post("/api/tickets").send({}).expect(401);
+});
+it("returns a status other than 401 if user signed in", async () => {
+  const response = await request(app).post("/api/tickets").send({});
+  expect(response.status).not.toEqual(401);
+});
+it("return an error if invalid title is provided", async () => {});
+it("return an error if invalid price is provided ", async () => {});
+it("create a ticket with valid input", async () => {});
